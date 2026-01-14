d209ffc9ef8d6d6aa434f7294fb0e3dcd6ca1d66
SKsergio <sergiofloress911@gmail.com>
2026-01-13 22:58:46 -0600

    debi haber intentado esto

--- todo-for-ale/resources/js/pages/settings/Appearance.vue
+++ todo-for-ale/resources/js/pages/settings/Appearance.vue
@@ -19,16 +19,14 @@ const breadcrumbItems: BreadcrumbItem[] = [
 
 <template>
     <AppLayout :breadcrumbs="breadcrumbItems">
+
         <Head title="Appearance settings" />
 
         <h1 class="sr-only">Appearance Settings</h1>
 
         <SettingsLayout>
             <div class="space-y-6">
-                <HeadingSmall
-                    title="Appearance settings"
-                    description="Update your account's appearance settings"
-                />
+                <HeadingSmall title="Appearance settings" description="Update your account's appearance settings" />
                 <AppearanceTabs />
             </div>
         </SettingsLayout>
